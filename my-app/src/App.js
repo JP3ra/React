38dc58dbc842f55b3a3e2b94d6903ff46254c295
JyotiprakashP <jyotiprakash2409@gmail.com>
2023-03-23 12:11:27 +0530

    Learnt how to implement dark and light mode

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
-import TextForm from './Components/TextForm';
+// import TextForm from './Components/TextForm';
+import About from './Components/About';
 
 function App() {
   return (
     <>
       <Navbar   />
       <div className="container">
-      <TextForm  heading = "This is the sample page" />
+      {/* <TextForm  heading = "This is the sample page" /> */}
+      <About />
       </div>
     </>
   );
